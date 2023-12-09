d8412a1a044d50e626dfb8106dc40a750fb8da42
El RIDO <elrido@gmx.net>
2023-12-09 13:51:56 +0100

    enable building on s390 arch

--- buildx.sh
+++ buildx.sh
@@ -22,7 +22,7 @@ build_image() {
 push_image() {
     # shellcheck disable=SC2068
     docker buildx build \
-        --platform linux/amd64,linux/386,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le \
+        --platform linux/amd64,linux/386,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/s390x \
         --pull \
         --no-cache \
         --push \
